a585bbe6b85213f9e73de2596f855f1526319fbe
Vladislav Sumin <vladislavsumin@mail.ru>
2025-06-30 21:39:26 +0300

    update window focus

--- home/.config/skhd/yabai_focus_window.sh
+++ home/.config/skhd/yabai_focus_window.sh
@@ -3,23 +3,23 @@
 # Этот скрипт ищет окно, приложение и заголовок которого соответствуют заданным параметрам,
 # и фокусирует его с помощью yabai.
 #
-# Использование: $0 <app-name> [title-regex]
+# Использование: $0 <app-regex> [title-regex]
 #
-# Пример 1: $0 "Firefox"
-# Пример 2: $0 "Firefox" "\\(vpn\\)"
+# Пример 1: $0 "^Firefox$"
+# Пример 2: $0 "^Firefox$" "\\(vpn\\)"
 
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 <app-name> [title-regex]"
+    echo "Usage: $0 <app-regex> [title-regex]"
     exit 1
 fi
 
-app_name="$1"
+app_regex="$1"
 title_regex="${2:-}"
 
 if [ -z "$title_regex" ]; then
-    window_id=$(yabai -m query --windows | jq -r --arg app "$app_name" '.[] | select(.app == $app) | .id' | head -n 1)
+    window_id=$(yabai -m query --windows | jq -r --arg app "$app_regex" '.[] | select(.app | test($app)) | .id' | head -n 1)
 else
-    window_id=$(yabai -m query --windows | jq -r --arg app "$app_name" --arg re "$title_regex" '.[] | select(.app == $app) | select(.title | test($re)) | .id' | head -n 1)
+    window_id=$(yabai -m query --windows | jq -r --arg app "$app_regex" --arg re "$title_regex" '.[] | select(.app | test($app)) | select(.title | test($re)) | .id' | head -n 1)
 fi
 
 if [ -n "$window_id" ]; then
